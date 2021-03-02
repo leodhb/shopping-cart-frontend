1ee04b02cfa1ceedd069fd5a9801db94cb85c338
leodhb <henrikleod@gmail.com>
2021-03-02 13:38:43 -0300

    Splitting pages and HoCs

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Header    from './components/Header/Header';
-import Products  from './components/Products/Products';
-import CartItem  from './components/Cart/CartItem/CartItem';
-import Preloader from './components/Shared/preloader';
-import ErrorBox  from './components/Shared/errorScreen';
-
+import Home from  './pages/home';
+import SessionContext from './contexts/SessionContext';
 import {loadSessionId} from './helpers/localStorage';
 
 const App = () => {
-  const [productList, setProductList] = useState([]);
-  const [cart, setCart]               = useState([]);
-  const [error, setError]             = useState(null);
-  const [loaded, isLoaded]            = useState(false);
-  const [sessionId, setSessionId]     = useState('teste de corno');
-
+  const [sessionId, setSessionId] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:3000/products")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setProductList(result);
-        isLoaded(true);
-        setError(null);
-        loadSessionId('@sallve-app/cartID').then((result) => setSessionId(result));
-      },
-      (error) => {
-        isLoaded(true);
-        setError(error.message);
-      }
-    )
+    loadSessionId('@sallve-app/ssessId').then(result => setSessionId(result));
   }, []);
 
-  const addToCartHandler = (sku, amount) => {
-      console.log('Adicionando ' + amount, sku + ' ao carrinho!');
-      console.log(sessionId);
-  }
-
-  let productDivContent = null;
-
-  if (error) {
-    productDivContent =  <ErrorBox error={error} ></ErrorBox>;
-  } else if (!loaded) {
-    productDivContent =  <Preloader></Preloader>;
-  } else {
-    productDivContent =  <Products list={productList} clicked={addToCartHandler} />
-  }
-
   return (
-    <div className="App">
-        <Header/>
-        <div className="container mt-3">
-        <div className="row sallve-MainPage">
-          <div className="col-lg-7 sallve-productList">
-            <div className="row">
-                {productDivContent}
-            </div>
-          </div>
-          <div className="col-lg-5 sallve-cartList">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                  <h3 className="mb-4 font-weight-bold">Sua sacola</h3>
-                  <CartItem></CartItem>
-                  <CartItem></CartItem>
-                  <CartItem></CartItem>
-                  <CartItem></CartItem>
-                  <CartItem></CartItem>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-        </div>
-    </div>
+      <Home sessionId={sessionId}/>
   );
 }
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect, useState } from 'react';
+import './styles.css';
+import ProductsContext from '../../contexts/ProductsContext';
+import CartContext from '../../contexts/CartContext';
+import Header from '../../components/Header';
+import ProductList from '../../components/ProductList';
+
+import api from '../../services/api';
+
+const Home = props => {
+
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const isSessionStarted = props.sessionId ? true : false;
+
+
+  useEffect( async () => {
+        const response = await api.get(`/products`);
+        const list = response.data;
+        setProducts(list);
+        console.log('lista de produtos',list);
+  }, []);
+
+  useEffect( async () => {
+    if(isSessionStarted) {
+        const response = await api.get(`/cart/${props.sessionId}`);
+        const list = response.data;
+        setCart(list);
+        console.log('carrinho',list);
+    }
+  }, [isSessionStarted]);
+
+
+  return (
+      <ProductsContext.Provider value={{products: products}} >
+          <CartContext.Provider value={{cart: cart}}>
+              <Header/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <ProductList></ProductList>
+                        </div>
+                        <div className="col-lg-6">
+                        </div>
+                    </div>
+                </div>
+          </CartContext.Provider>
+      </ProductsContext.Provider>
+  );
+}
+
+export default Home;
