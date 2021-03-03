6c729f5d7a14c572e0ea0acd2c8545423f9591d3
leodhb <henrikleod@gmail.com>
2021-03-03 05:16:31 -0300

    Adjust header size

--- src/ui/components/Header/index.jsx
+++ src/ui/components/Header/index.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/img/logo-sallve.png';
 
 const Header = () => {
     return (
-        <header className="row sallve-header">
+        <header className="row sallve-header mb-4">
                 <img src={logo} className="sallve-brand" alt="Logomarca da Sallve"/>
         </header>
     )
