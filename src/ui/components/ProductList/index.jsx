b97b19256bf1b96cad256fac425e2687a2d707eb
leodhb <henrikleod@gmail.com>
2021-03-03 05:24:56 -0300

    Splitting loaded list and preloader

--- src/ui/components/ProductList/index.jsx
+++ src/ui/components/ProductList/index.jsx
@@ -2,16 +2,23 @@ import React, { useContext } from 'react';
 import './styles.css';
 import Product from '../Product';
 import {ProductsContext} from '../../contexts/ProductsContext';
+import ErrorScreen from '../../screens/ErrorScreen';
+import PreloaderScreen from '../../screens/PreloaderScreen';
 
 const ProductList = () => {
-    const {products} = useContext(ProductsContext);
+    const {products, productsError, isProductsLoaded} = useContext(ProductsContext);
+
+    const productListLoaded = 
+    (<div className="row">
+        {(!productsError) ? products.map(product => {
+            return <Product key={product._id} sku={product.sku[0].id} name={product.name} image={product.image} price={product.sku[0].price.toFixed(2)} />
+        }) : <ErrorScreen/>}
+    </div>)
 
     return (
-        <div className="row">
-            {products.map(product => {
-                return <Product key={product._id} sku={product.sku[0].id} name={product.name} image={product.image} price={product.sku[0].price.toFixed(2)} />
-            })}
-        </div>
+        <>
+           {isProductsLoaded ? productListLoaded : <PreloaderScreen/>}
+        </>
     )
 }
 
