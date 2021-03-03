6e556404d36b456a172482c5d19ab6aabbae62c4
leodhb <henrikleod@gmail.com>
2021-03-02 21:01:59 -0300

    Define SessionContext functions

--- src/contexts/SessionContext.js
+++ src/contexts/SessionContext.js
@@ -1,7 +1,20 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import {loadSessionId} from '../helpers/localStorage';
 
-const SessionContext = createContext({
-    sessionId: ''
-});
+export const SessionContext = createContext();
 
-export default SessionContext;
+const SessionContextProvider = ({children}) => {
+    const [sessionId, setSessionId] = useState('');
+
+    useEffect(() => {
+        loadSessionId('@sallve-app/ssessId').then(result => {setSessionId(result); console.log('SESSÃO: ', result)});
+    }, []);
+
+ return (
+    <SessionContext.Provider value={{sessionId, setSessionId}}>
+        {children}
+    </SessionContext.Provider>
+ )
+}
+
+export default SessionContextProvider;
