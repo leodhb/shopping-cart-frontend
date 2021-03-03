340a198229b9baf161264e0ef1c424d2b12ef24c
leodhb <henrikleod@gmail.com>
2021-03-03 05:15:27 -0300

    Cleaning...

--- src/ui/pages/home/index.js
+++ src/ui/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import './styles.css';
 import Header      from '../../components/Header';
 import ProductList from '../../components/ProductList';
