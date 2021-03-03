5e9ddb249b8c8667f476d69faf5fd664ebeef797
leodhb <henrikleod@gmail.com>
2021-03-02 21:02:22 -0300

    Define CartContext functions

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,50 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import {SessionContext} from './SessionContext';
+import api from '../services/api';
 
-const CartContext = createContext({
-    cart: []
-});
+export const CartContext = createContext();
 
-export default CartContext;
+const CartContextProvider = ({children}) => {
+    const [cart, setCart] = useState([]);
+    const [isCartLoaded, setCartLoaded] = useState(false);
+    const {sessionId} = useContext(SessionContext);
+    
+    useEffect( async () => {
+        if(sessionId) {
+            const response = await api.get(`/cart/${sessionId}`);
+            const list = response.data;
+            setCart(list);
+            setCartLoaded(true);
+            console.log('MEU CARRINHO: ',list);
+        }
+    }, [sessionId]);
+
+    const addToCart = async (sku, amount) => {
+        const response = await api.put(`/cart/${sessionId}`, { sku: sku, qty: amount });
+        const list = response.data;
+        setCart(list);
+        console.log('MEU CARRINHO DEPOIS DE ADICIONAR PRODUTO: ',list);
+    }
+
+    const updateCart = async (sku, amount) => {
+        const response = await api.patch(`/cart/${sessionId}`, { sku: sku, qty: amount });
+        const list = response.data;
+        setCart(list);
+        console.log('MEU CARRINHO DEPOIS DE MODIFICAR PRODUTO: ',list);
+    }
+
+    const deleteFromCart = async (sku) => {
+        const response = await api.delete(`/cart/${sessionId}/${sku}`);
+        const list = response.data;
+        setCart(list);
+        console.log('MEU CARRINHO DEPOIS DE DELETAR PRODUTO: ',list);
+    }
+
+ return (
+        <CartContext.Provider value={{cart,isCartLoaded, setCart, addToCart, updateCart, deleteFromCart}}>
+            {children}
+        </CartContext.Provider>
+ )
+}
+
+export default CartContextProvider;
