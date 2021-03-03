defb67d787cae95cc045050faab2b2f6d37b15ee
leodhb <henrikleod@gmail.com>
2021-03-02 21:02:46 -0300

    Change static list to dynamic list from Provider

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles.css';
 import Product from '../Product';
+import {ProductsContext} from '../../contexts/ProductsContext';
 
 const ProductList = () => {
+    const {products} = useContext(ProductsContext);
+
     return (
         <div className="row">
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
+            {products.map(product => {
+                return <Product key={product._id} sku={product.sku[0].id} name={product.name} image={product.image} price={product.sku[0].price.toFixed(2)} />
+            })}
         </div>
     )
 }
