7a6bae4f97308883931955c6d3dcf7033211c71a
leodhb <henrikleod@gmail.com>
2021-03-03 05:14:44 -0300

    Set Error Screen final layout

---
+++ src/ui/screens/ErrorScreen/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import ErrorIcon from '../../../assets/img/error_icon.png';
+
+const ErrorScreen = () => {
+    return (
+        <div className="error-container">
+            <img className="error-icon" src={ErrorIcon} alt="Houve um problema com a página"/>
+            <h4 className="text-secondary">ops! tivemos um probleminha para carregar esta página. </h4>
+        </div>
+    )
+}
+
+export default ErrorScreen; 
