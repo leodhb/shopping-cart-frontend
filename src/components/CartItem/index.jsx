6d265fbc183409b04d2bffe25ea47d73eb774225
leodhb <henrikleod@gmail.com>
2021-03-02 21:03:19 -0300

    Change static values to Provider Values

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles.css';
 import CloseIcon from '../../assets/img/close_icon.png';
-const CartItem = () => {
+import {CartContext} from '../../contexts/CartContext';
+
+const CartItem = props => {
+    const {SKU, qty, totalItemValue} = props.item;
+    const {name, image} = props.product;
+    const {updateCart, deleteFromCart} = useContext(CartContext);
+    
     return (
         <tr className="cart-item">
                 <td className="cart-img-container">
-                  <img className="mx-auto" src="https://cdn.shopify.com/s/files/1/0074/3486/2639/products/sallve-kitsfevereiro-kit-antiacne-mini-hover_grande.jpg"/>
+                  <img className="mx-auto" src={image} />
                 </td>
                 <td>
                   <div className="cart-title-bar">
-                        <h4>Kit tudooo</h4>
-                        <img src={CloseIcon} className="delete-product-btn" alt=""/>
+                        <h4>{name}</h4>
+                        <img src={CloseIcon} onClick={() => deleteFromCart(SKU)} className="delete-product-btn" alt=""/>
                   </div>
-                  
+
                   <div className="cart-amount-bar">
-                    <button className="change-qty-btn mx-1" type="button">-</button>
-                    <span className="qty-amount">1</span>
-                    <button className="change-qty-btn mx-1" type="button">+
+                    <button onClick={() => updateCart(SKU, -1)} className="change-qty-btn mx-1" type="button">-</button>
+                    <span className="qty-amount">{qty}</span>
+                    <button onClick={() => updateCart(SKU, 1)} className="change-qty-btn mx-1" type="button">+
                     </button>
 
-                    <h5 className="cart-item-price my-auto ml-4">R$ 79.90</h5>
+                    <h5 className="cart-item-price my-auto ml-4">R$ {totalItemValue}</h5>
                   </div>
                 </td>
               </tr>
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles.css';
+import {CartContext} from '../../contexts/CartContext';
 
-const Product = () => {
+const Product = props => {
+    const {addToCart} = useContext(CartContext);
+
     return (
         <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 mb-4">
             <div className="card product-card">
-                <img src="https://cdn.shopify.com/s/files/1/0074/3486/2639/products/sallve-kitsfevereiro-kit-antiacne-mini-hover_grande.jpg" className="card-img-top"  alt="Foto de "></img>
+                <img src={props.image} className="card-img-top"  alt={props.name}></img>
                 <div className="card-body px-0">
-                    <h6>R$  79.90 </h6>
-                    <h5 className="product-title">Kit massa</h5>
-                    <button className="comprar-btn">comprar</button>
+                    <h6>R$ {props.price} </h6>
+                    <h5 className="product-title">{props.name}</h5>
+                    <button onClick={() => addToCart(props.sku, 1)} className="comprar-btn">comprar</button>
                 </div>
             </div>
         </div>
