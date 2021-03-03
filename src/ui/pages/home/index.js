a7fd5a4828817b99ef2a9d6235b5257399b834f5
leodhb <henrikleod@gmail.com>
2021-03-03 04:11:18 -0300

    Splitting responsabilities

--- src/ui/pages/home/index.js
+++ src/ui/pages/home/index.js
@@ -4,22 +4,7 @@ import Header      from '../../components/Header';
 import ProductList from '../../components/ProductList';
 import Cart        from '../../components/Cart';
 
-import {CartContext} from '../../contexts/CartContext';
-
 const Home = () => {
-  const {cart, isCartLoaded} = useContext(CartContext);
-  const [cartDiv, setCartDiv]  = useState();
-
-
-  useEffect(() => {
-      if(isCartLoaded) {
-          if(cart.totalCartValue === 0) {
-              setCartDiv(<h1>Carrinho zerado brou</h1>)
-          } else {
-              setCartDiv(<Cart></Cart>)
-          }
-      }
-  }, [cart, isCartLoaded]);
 
   return (
             <>
@@ -30,7 +15,7 @@ const Home = () => {
                             <ProductList></ProductList>
                         </div>
                         <div className="col-lg-6">
-                            {cartDiv}
+                            <Cart></Cart>
                         </div>
                     </div>
                 </div>
