b0c1a0e8e01cbcbb50436ce6fae58bb725e9b32a
leodhb <henrikleod@gmail.com>
2021-03-02 21:02:13 -0300

    Define ProductsContext functions

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,7 +1,23 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import api from '../services/api';
 
-const ProductsContext = createContext({
-    products: []
-});
+export const ProductsContext = createContext();
 
-export default ProductsContext;
+const ProductContextProvider = ({children}) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect( async () => {
+        const response = await api.get(`/products`);
+        const list = response.data;
+        setProducts(list);
+    }, []);
+
+ return (
+    <ProductsContext.Provider value={{products, setProducts}}>
+        {children}
+    </ProductsContext.Provider>
+ )
+}
+
+
+export default ProductContextProvider;
